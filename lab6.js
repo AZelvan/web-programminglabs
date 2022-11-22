b99f313b7d61d048aaff103de826d201eb572850
Alina i Veronika <AlinaZelvam@gmail.com>
2022-11-22 10:17:24 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -37,3 +37,32 @@ function setEnglish() {
     document.getElementById('reset-button').value = 'reset';
     
     }
+    function showAlert () {
+        let userName = document.getElementById('input-name').value;
+        let userSurname = document.getElementById('input-surname').value;
+        let userAge = document.getElementById('input-age').value;
+        alert ('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+        let r = Math.round(Math.random()*255);
+        let g = Math.round(Math.random()*255);
+        let b = Math.round(Math.random()*255);
+        let color = 'rgb(' +  r + ',' + g + ',' + b + ')';
+        document.getElementById('user-date').style.backgroundColor=color;
+    }
+
+    function showHead () {
+        let r = Math.round(Math.random()*255);
+        let g = Math.round(Math.random()*255);
+        let b = Math.round(Math.random()*255);
+        let color = 'rgb(' +  r + ',' + g + ',' + b + ')';
+        document.getElementById('student').style.backgroundColor=color;
+        alert ('Привет, это мы' );
+    }
+
+    function showFoot () {
+        let r = Math.round(Math.random()*255);
+        let g = Math.round(Math.random()*255);
+        let b = Math.round(Math.random()*255);
+        let color = 'rgb(' +  r + ',' + g + ',' + b + ')';
+        document.getElementById('footer').style.backgroundColor=color;
+        alert ('Привет, это тоже мы' );
+    }
