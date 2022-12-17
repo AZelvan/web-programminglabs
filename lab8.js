63ab35a5ed56494ce2cb44a6c911ce775cfc1f03
Alina i Veronika <AlinaZelvam@gmail.com>
2022-12-17 09:34:09 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -16,3 +16,14 @@ function showDaysCount(params) {
 }
 
 window.addEventListener('load', showDate);
+
+function showTime (){
+    let outTime = document.getElementById ('time');
+    let currentTime = new Date ();
+    outTime.innerHTML = currentTime.toLocaleTimeString ('ru');
+}
+
+window.addEventListener('load', showTime);
+setInterval (showTime, 1000);
+
+
