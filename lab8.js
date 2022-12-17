29937c9329e96f2ab5a22f5f8162639409a99d06
Alina i Veronika <AlinaZelvam@gmail.com>
2022-12-17 09:04:35 +0700

    Вывод даты

---
+++ lab8.js
@@ -0,0 +1,8 @@
+function showDate (params){
+    let out = document.getElementById ('today');
+    let today = new Date ();
+    out.innerHTML = 'Сегодня ' + today.toLocaleDateString ("ru-RU");
+
+}
+
+window.addEventListener('load', showDate);
