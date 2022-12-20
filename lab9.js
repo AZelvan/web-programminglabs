c5df459496a6e360be5145b7b51147c49ec3e206
Alina i Veronika <AlinaZelvam@gmail.com>
2022-12-20 09:46:17 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -10,3 +10,21 @@ function clickMe (button) {
 	else
 		button.innerHTML = 'Щелчок';
 }
+
+function Swap (button) {
+	if (button.innerHTML =='Нажми меня') {
+		button.innerHTML='Еще раз нажми';
+        button.style.color= 'green';}
+    else   
+    if (button.innerHTML =='Еще раз нажми') {
+		button.innerHTML='Еще';
+        button.style.color= 'blue';}
+    else
+    if (button.innerHTML =='Еще') {
+		button.innerHTML='Начать сначала';
+        button.style.color= 'red';}
+    else
+    if (button.innerHTML =='Начать сначала') {
+		button.innerHTML='Нажми меня';
+        button.style.color= 'white';}
+}
