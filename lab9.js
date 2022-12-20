4bdcf385ff23574d030c469eb100e194bec41b12
Alina i Veronika <AlinaZelvam@gmail.com>
2022-12-20 09:30:37 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -1,6 +1,12 @@
 function clickMe (button) {
 	if (button.innerHTML =='Щелчок')
-		button.innerHTML=='Спасибо';
-		else
+		button.innerHTML='Спасибо';
+    else
+    if (button.innerHTML =='Имя Алина')
+		button.innerHTML='Фамилия Алина';
+    else
+    if (button.innerHTML =='Имя Вероника')
+		button.innerHTML='Фамилия Вероника';
+	else
 		button.innerHTML = 'Щелчок';
 }
